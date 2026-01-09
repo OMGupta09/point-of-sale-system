1b1081c7e5fd4a7158f260132312d2dec2d0b649
OM GUPTA <146474653+OMGupta09@users.noreply.github.com>
2026-01-09 17:28:33 +0530

    Updated User Controller

--- ogPOSsystem/src/main/java/com/ogcodes/ogPOSsystem/controller/UserController.java
+++ ogPOSsystem/src/main/java/com/ogcodes/ogPOSsystem/controller/UserController.java
@@ -7,22 +7,34 @@ import com.ogcodes.ogPOSsystem.payload.dto.Userdto;
 import com.ogcodes.ogPOSsystem.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
+
+    @GetMapping("/profile")
     public ResponseEntity<Userdto> getUserProfile(
             @RequestHeader("Authorization") String jwt
     ) throws UserException {
         User user=userService.getUserFromJwtToken(jwt);
-        return ResponseEntity.ok(UserMapper.toDTO(user)    );
+        return ResponseEntity.ok(UserMapper.toDTO(user));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Userdto> getUserById(
+            @RequestHeader("Authorization") String jwt,
+            @PathVariable Long id
+    ) throws UserException {
+        User user=userService.getUserById(id);
+        if(user==null)
+        {
+            throw new UserException("User Not Found");
+        }
+        return ResponseEntity.ok(UserMapper.toDTO(user));
     }
 }
